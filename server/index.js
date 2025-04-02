4daaf206b5eb2a755a21b453fad567c792378402
Manthan Bire <manthansharadbire@gmail.com>
2025-04-02 14:11:49 +0530

    create and read

--- server/index.js
+++ server/index.js
@@ -3,36 +3,46 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
+import WweSuperstar from './models/WweSuperstar.js'
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const connectDB = ()=>{
-    const conn = mongoose.connect(process.env.MONGODB_URI)
+const connectDB = async()=>{
+    const conn = await mongoose.connect(process.env.MONGODB_URI)
     if(conn){
         console.log("MongoDB connection successfull")
     }
 }
-
-const WWE_SUPERSTARS = []
 
 app.get("/health", (req,res)=>{
     res.status(200).json({meassage:"Server is Healthy !"})
 });
 
 app.get("/wwe-superstars", (req,res)=>{
+
+    const WweSuperstar = WweSuperstar.find();
     return res.status(200).json({
         success:true,
-        data:WWE_SUPERSTARS,
+        data:WweSuperstar,
         message:"WWE Superstar fetched successfully"
     })
 });
 
-app.post("/wwe-superstars",(req,res)=>{
+app.post("/wwe-superstars",async(req,res)=>{
+
+    const {wwename, height, finisher, aka, thumbnail} = req.body;
+
+    const newWweSuperstar = new WweSuperstar({
+        wwename, height, finisher, aka, thumbnail
+    });
+
+    const savedWweSuperstars = await newWweSuperstar.save();
+    
     return res.status(201).json({
         success:true,
-        data:WWE_SUPERSTARS,
+        data:savedWweSuperstars,
         message:"WWE Superstar created successfully"
     })
 })
--- server/models/WweSuperstar.js
+++ server/models/WweSuperstar.js
@@ -1,2 +1,13 @@
 import { Schema, model } from "mongoose";
 
+const wweSuperstarSchema = new Schema({
+    wwename:String, 
+    height:String, 
+    finisher:String,
+    aka:String, 
+    thumbnail:String,
+});
+
+const WweSuperstar = model("WweSuperstar",wweSuperstarSchema );
+
+export default WweSuperstar;
