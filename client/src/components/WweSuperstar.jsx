9e76de449fc83508fc42f0048b9e3a407d88bbc5
Manthan Bire <manthansharadbire@gmail.com>
2025-04-03 13:54:33 +0530

    integreated backend with frontend

---
+++ client/src/components/WweSuperstar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function WweSuperstar({
+    _id, wwename, height, finisher, aka, thumbnail
+}) {
+  return (
+    <div>
+  <h2>{wwename}</h2>
+  <h2>{height}</h2>
+    </div>
+  )
+}
+
+export default WweSuperstar;
--- client/src/views/Homepage.jsx
+++ client/src/views/Homepage.jsx
@@ -1,31 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
-const WWE_SUPERSTARS = ([
-    {
-        "_id": "67ecf8ceb364ccd14a1d2355",
-        "wwename": "Roman Reigns",
-        "height": "6.1ft",
-        "finisher": "Prodigal Spear",
-        "aka": "Tribal Chief",
-        "thumbnail": "https://www.usanetwork.com/sites/usablog/files/2022/09/wwe-roman-reigns-titles.jpg",
-        "__v": 0
-    },
-    {
-        "_id": "67ecf925b364ccd14a1d2357",
-        "wwename": "Seth Rollins",
-        "height": "6.1ft",
-        "finisher": "Curb Stomp",
-        "aka": "The Architect",
-        "thumbnail": "https://cdn.wrestletalk.com/wp-content/uploads/2023/07/seth-rollins-wwe-world-heavyweight-champion-july-2-b.jpg",
-        "__v": 0
-    }
-])
+import WweSuperstar from  '../components/WweSuperstar.jsx'
 
 function Homepage() {
+
+    const [wweSuperstars , setWweSuperstars] = useState([]);
+
+    const loadWweSuperstars = async() =>{
+        const response = await axios.get("http://localhost:5002/wwe-superstars");
+
+        setWweSuperstars(response.data.data);
+    };
+
+    useEffect(()=>{
+        loadWweSuperstars();
+    },[]);
+
   return (
     <div>
       <h1>This is Homepage</h1>
+      {wweSuperstars.map((wrestler, i)=>{
+        const{_id, wwename, height, finisher, aka, thumbnail} = wrestler;
+        return(
+           <WweSuperstar
+           key={i}
+           _id={_id}
+           wwename={wwename}
+           height={height}
+           finisher={finisher}
+           aka={aka}
+           thumbnail={thumbnail}
+           />
+        )
+      })}
     </div>
   )
 }
