512fb223a771058ef2e952b1f0a55b7b8f5123a0
Manthan Bire <manthansharadbire@gmail.com>
2025-04-06 12:03:51 +0530

    Homepage component rendered on UI

--- client/src/components/WweSuperstar.jsx
+++ client/src/components/WweSuperstar.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from "react";
 
-function WweSuperstar({
-    _id, wwename, height, finisher, aka, thumbnail
-}) {
+function WweSuperstar({ _id, wwename, height, finisher, aka, thumbnail }) {
   return (
     <div>
-  <h2>{wwename}</h2>
-  <h2>{height}</h2>
+      <img src={thumbnail} alt="" />
+      <h2>{wwename}</h2>
+      <h2>{height}</h2>
+      <h3>{finisher}</h3>
+      <p>{aka}</p>
     </div>
-  )
+  );
 }
 
 export default WweSuperstar;
--- client/src/views/Homepage.jsx
+++ client/src/views/Homepage.jsx
@@ -1,40 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import WweSuperstar from  '../components/WweSuperstar.jsx'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import WweSuperstar from "../components/WweSuperstar.jsx";
 
 function Homepage() {
+  const [wweSuperstars, setWweSuperstars] = useState([]);
 
-    const [wweSuperstars , setWweSuperstars] = useState([]);
+  const loadWweSuperstars = async () => {
+    const response = await axios.get(`${import.meta.env.VITE_API_URI}/wwe-superstars`);
 
-    const loadWweSuperstars = async() =>{
-        const response = await axios.get("http://localhost:5002/wwe-superstars");
+    setWweSuperstars(response.data.data);
+  };
 
-        setWweSuperstars(response.data.data);
-    };
-
-    useEffect(()=>{
-        loadWweSuperstars();
-    },[]);
+  useEffect(() => {
+    loadWweSuperstars();
+  }, []);
 
   return (
     <div>
       <h1>This is Homepage</h1>
-      {wweSuperstars.map((wrestler, i)=>{
-        const{_id, wwename, height, finisher, aka, thumbnail} = wrestler;
-        return(
-           <WweSuperstar
-           key={i}
-           _id={_id}
-           wwename={wwename}
-           height={height}
-           finisher={finisher}
-           aka={aka}
-           thumbnail={thumbnail}
-           />
-        )
+      {wweSuperstars.map((wrestler, i) => {
+        const { _id, wwename, height, finisher, aka, thumbnail } = wrestler;
+        return (
+          <WweSuperstar
+            key={i}
+            _id={_id}
+            wwename={wwename}
+            height={height}
+            finisher={finisher}
+            aka={aka}
+            thumbnail={thumbnail}
+          />
+        );
       })}
     </div>
-  )
+  );
 }
 
 export default Homepage;
